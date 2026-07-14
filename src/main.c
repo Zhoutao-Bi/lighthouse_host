1551989ec6df6ed183d7338f2358dec2b2ab6e89
Zhoutao-Bi <bizhoutao.terry@gmail.com>
2026-07-14 22:31:09 +0800

    初版

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <zephyr/kernel.h>
 
+#include "led.h"
+
 int main(void)
 {
-        return 0;
+	int err = led_init();
+
+	if (err) {
+		return err;
+	}
+
+	return led_blink(LED_DEFAULT_PERIOD_MS);
 }
